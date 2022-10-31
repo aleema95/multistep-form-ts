ee092eb6a2f9b812c0202adb26edd85286ec54fd
aleema95 <alejandro.manouellian@gmail.com>
2022-10-31 11:17:14 -0300

    added css to form

--- src/AccountForm.tsx
+++ src/AccountForm.tsx
@@ -1,4 +1,5 @@
 import { FormWrapper } from "./FormWrapper";
+import s from './Forms.module.css'
 
 interface AccountData {
   email: string,
@@ -12,21 +13,25 @@ interface AccountFormProps extends AccountData {
 export function AccountForm({ email, password, updateFields }: AccountFormProps) {
   return (
     <FormWrapper title="Account Creation">
-      <label>Email</label>
-      <input 
-        autoFocus 
-        required 
-        type="email" 
-        value={email} 
-        onChange={ e => updateFields({ email: e.target.value})} 
-      />
-      <label>Password</label>
-      <input 
-        required 
-        type="password" 
-        value={password} 
-        onChange={ e => updateFields({ password: e.target.value})} 
-      />
+      <div className={s.inputContainer}>
+        <label>Email</label>
+        <input 
+          autoFocus 
+          required 
+          type="email" 
+          value={email} 
+          onChange={ e => updateFields({ email: e.target.value})} 
+        />
+      </div>
+      <div className={s.inputContainer}>
+        <label>Password</label>
+        <input 
+          required 
+          type="password" 
+          value={password} 
+          onChange={ e => updateFields({ password: e.target.value})} 
+        />
+      </div>
     </FormWrapper>
   )
 }
--- src/AddressForm.tsx
+++ src/AddressForm.tsx
@@ -1,4 +1,5 @@
 import { FormWrapper } from "./FormWrapper";
+import s from './Forms.module.css'
 
 interface AddressData {
   street: string,
@@ -14,35 +15,43 @@ interface AddressFormProps extends AddressData {
 export function AddressForm({ street, city, state, zip, updateFields}: AddressFormProps) {
   return (
     <FormWrapper title="Address">
-      <label>Street</label>
-      <input 
-        autoFocus 
-        required 
-        type="text" 
-        value={street} 
-        onChange={ e => updateFields({ street: e.target.value})} 
-      />
-      <label>City</label>
-      <input 
-        required 
-        type="text"  
-        value={city} 
-        onChange={ e => updateFields({ city: e.target.value})}
-      />
-      <label>State</label>
-      <input 
-        required 
-        type="text" 
-        value={state} 
-        onChange={ e => updateFields({ state: e.target.value})} 
-      />
-      <label>Zip</label>
-      <input 
-        required 
-        type="text" 
-        value={zip} 
-        onChange={ e => updateFields({ zip: e.target.value})} 
-      />
+      <div className={s.inputContainer}>
+        <label>Street</label>
+        <input 
+          autoFocus 
+          required 
+          type="text" 
+          value={street} 
+          onChange={ e => updateFields({ street: e.target.value})} 
+        />
+      </div>
+      <div className={s.inputContainer}>
+        <label>City</label>
+        <input 
+          required 
+          type="text"  
+          value={city} 
+          onChange={ e => updateFields({ city: e.target.value})}
+        />
+      </div>
+      <div className={s.inputContainer}>
+        <label>State</label>
+        <input 
+          required 
+          type="text" 
+          value={state} 
+          onChange={ e => updateFields({ state: e.target.value})} 
+        />
+      </div>
+      <div className={s.inputContainer}>
+        <label>Zip</label>
+        <input 
+          required 
+          type="text" 
+          value={zip} 
+          onChange={ e => updateFields({ zip: e.target.value})} 
+        />
+      </div>
     </FormWrapper>
   )
 }
